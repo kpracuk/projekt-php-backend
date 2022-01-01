3fc3e227a2b197cd9246c21cc3224c85296135b6
kpracuk <skeezze@gmail.com>
2022-01-01 19:51:42 +0100

    configured auth endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
+
+Route::get('/auth/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth');
