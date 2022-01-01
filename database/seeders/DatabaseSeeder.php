baf3638a5391856e73c93404151fd3ec8d203cd5
kpracuk <skeezze@gmail.com>
2022-01-01 21:40:42 +0100

    added products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'email_verified_at' => now(),
+             'password' => Hash::make('123123123')
+         ]);
+         User::factory(10)->create();
+         Product::factory(20)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/auth/user', function (Request $request) {
     return $request->user();
 })->middleware('auth');
+
+Route::resource('/api/products', \App\Http\Controllers\ProductController::class)->only(['index']);
